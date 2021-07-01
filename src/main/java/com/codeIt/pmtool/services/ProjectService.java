142b49e51b6e04daed960fd68891882616452e9d
Kok Wai Chan <kokwai2107@gmail.com>
2021-07-01 13:49:58 +0800

    Created exception Class for Id & Modified ProjectService for exception

--- src/main/java/com/codeIt/pmtool/services/ProjectService.java
+++ src/main/java/com/codeIt/pmtool/services/ProjectService.java
@@ -2,6 +2,7 @@ package com.codeIt.pmtool.services;
 
 import com.codeIt.pmtool.model.Project;
 import com.codeIt.pmtool.repository.ProjectRepository;
+import exceptions.ProjectIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,13 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdate(Project project){
-        return projectRepository.save(project);
+    public Project saveOrUpdate(Project project) {
+
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
